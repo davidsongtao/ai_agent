27a3a9f4affda7d13b8da1e9d680aaaf6f72e82a
King Songtao <king.songtao@gmail.com>
2024-10-08 10:25:13 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+Description: 整体项目全局配置文件，用于配置全局参数
+    
+-*- Encoding: UTF-8 -*-
+@File     ：config.py
+@Author   ：King Songtao
+@Time     ：2024/10/8 上午10:21
+@Contact  ：king.songtao@gmail.com
+"""
+
+
+class ParametersConfig(object):
+    """全局参数配置"""
+
+    def __init__(self):
+        self.log_directory = r"D:\ai_agent\logs"
